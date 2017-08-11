6c181a8c6ab35dbe7dcc64b201fd02a3ab1d5a2a
BtoBastian <bastianoppermann1997@gmail.com>
2017-08-11 12:50:19 +0200

    Added SudokuType enum

---
+++ src/main/java/de/btobastian/sudoku/SudokuType.java
@@ -0,0 +1,25 @@
+package de.btobastian.sudoku;
+
+/**
+ * The type of the sudoku.
+ */
+public enum SudokuType {
+
+    /**
+     * A normal sudoku.
+     */
+    NORMAL(),
+
+    /**
+     * A samurai sudoku.
+     */
+    SAMURAI();
+
+    /**
+     * Class constructor.
+     */
+    SudokuType() {
+
+    }
+
+}
