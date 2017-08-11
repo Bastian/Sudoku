57ae920d8cd6bc6eb6a938b797221b523535911f
BtoBastian <bastianoppermann1997@gmail.com>
2017-08-11 13:56:30 +0200

    Added SudokuDifficulty enum

---
+++ src/main/java/de/btobastian/sudoku/SudokuDifficulty.java
@@ -0,0 +1,40 @@
+package de.btobastian.sudoku;
+
+/**
+ * The difficulty of a sudoku.
+ */
+public enum SudokuDifficulty {
+
+    /**
+     * A very easy sudoku.
+     */
+    VERY_EASY(),
+
+    /**
+     * An easy sudoku.
+     */
+    EASY(),
+
+    /**
+     * A normal sudoku.
+     */
+    NORMAL(),
+
+    /**
+     * A hard sudoku.
+     */
+    HARD(),
+
+    /**
+     * A very hard sudoku.
+     */
+    VERY_HARD();
+
+    /**
+     * Class constructor.
+     */
+    SudokuDifficulty() {
+
+    }
+
+}
