d22644a9a09b974c78cf65b9855672eb33d65f8e
BtoBastian <bastianoppermann1997@gmail.com>
2017-08-11 13:56:01 +0200

    Added block size to SudokuSize JavaDocs

--- src/main/java/de/btobastian/sudoku/SudokuSize.java
+++ src/main/java/de/btobastian/sudoku/SudokuSize.java
@@ -6,32 +6,32 @@ package de.btobastian.sudoku;
 public enum SudokuSize {
 
     /**
-     * A tiny sudoku with a 4x4 grid size.
+     * A tiny sudoku with a 4x4 grid size (2x2 blocks).
      */
     TINY(4),
 
     /**
-     * A normal sudoku with a 9x9 grid size.
+     * A normal sudoku with a 9x9 grid size (3x3 blocks).
      */
     NORMAL(9),
 
     /**
-     * A large sudoku with a 16x16 grid size.
+     * A large sudoku with a 16x16 grid size (4x4 blocks).
      */
     LARGE(16),
 
     /**
-     * A huge sudoku with a 25x25 grid size.
+     * A huge sudoku with a 25x25 grid size (5x5 blocks).
      */
     HUGE(25),
 
     /**
-     * An extreme sudoku with a 36x36 grid size.
+     * An extreme sudoku with a 36x36 grid size (6x6 blocks).
      */
     EXTREME(36),
 
     /**
-     * An enormous sudoku with a 49x49 grid size.
+     * An enormous sudoku with a 49x49 grid size (7x7 blocks).
      */
     ENORMOUS(49);
 
